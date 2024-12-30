a1ea33e84f594f622918fc230ac2d9167773dba2
Mehmet <mehmettemizkanabs@gmail.com>
2024-12-30 23:47:49 +0100

    String Metodlar4

---
+++ src/benimcalismalarim/yilbasitatili/StrinTumMetodlari.java
@@ -0,0 +1,22 @@
+package benimcalismalarim.yilbasitatili;
+
+public class StrinTumMetodlari {
+    public static void main(String[] args) {
+        // Soru: Verilen bir string'in uzunluğunu döndüren bir metot yazınız.
+        // Örneğin, "merhaba" verildiğinde 7 döndürülmelidir.
+        String cumle = "Java ögrendikce mutluluktur";
+        System.out.println("1- Stringin uzunlugu  = " + cumle.length());
+
+        //Soru: Verilen bir string'den belirli bir başlangıç ve bitiş indeksine göre alt dizeyi döndüren bir metot yazınız.
+        // Örneğin, "merhaba dünya" verildiğinde 3. indeksten 8. indekse kadar olan alt dizenin "haba" olmasını bekleriz.
+
+
+
+
+
+
+
+
+
+    }
+}
