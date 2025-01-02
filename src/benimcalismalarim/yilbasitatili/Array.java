b82521727a489ab1b3950f74517a0ab8143c7245
Mehmet <mehmettemizkanabs@gmail.com>
2025-01-02 02:06:01 +0100

    Arrays

--- src/benimcalismalarim/yilbasitatili/Array.java
+++ src/benimcalismalarim/yilbasitatili/Array.java
@@ -1,4 +1,71 @@
 package benimcalismalarim.yilbasitatili;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Array {
+    public static void main(String[] args) {
+
+        int [] sayilar = new int[]{20,80,20,60,20};
+
+        System.out.println("Sayilar Arrayi = " +Arrays.toString(sayilar));
+        System.out.println(sayilar [1]);
+
+        int enBuyuk = sayilar[0];
+        int enKucuk = sayilar[0];
+
+        for (int sayi : sayilar) {
+            if (sayi > enBuyuk) enBuyuk = sayi;
+            if (sayi < enKucuk) enKucuk = sayi;
+        }
+
+        System.out.println("En Büyük: " + enBuyuk);
+        System.out.println("En Küçük: " + enKucuk);
+
+        int hedef = 20;
+        int i =0;
+        for (int sayi : sayilar){
+            if ( sayi==hedef){
+
+                i++;
+            }
+        }
+        System.out.println("Hedef " + hedef + " sayısı " + i + " kez tekrar ediyor.");
+
+        String [] hayvanlar = new String[5];
+        hayvanlar[0] = "Kedi";
+        hayvanlar[2] = "Keci";
+        hayvanlar[4] = "Kaplan";
+
+        System.out.println("Sayilar Arrayi = " +Arrays.toString(hayvanlar));
+
+        String [] hayvanlar1 = new String []{"Kedi", "Köpek", "Kaplan", "Fil", "Zebra", "Yilan", "Fare"};
+        System.out.println("Hayvanlar Arrayi = " +Arrays.toString(hayvanlar1));
+
+        //Array Metodlari
+        //1- sort
+       Arrays.sort(hayvanlar1);
+        System.out.println(Arrays.toString(hayvanlar1));
+
+        int indeks = Arrays.binarySearch(hayvanlar1, "Yilan");
+        System.out.println("Aranan kelimenin indeksi: " + indeks);
+
+        String [] yeniHayvalarList = Arrays.copyOf(hayvanlar1, 8);
+        System.out.println(Arrays.toString(yeniHayvalarList));
+
+        String [] yeniHayvalarListOls = Arrays.copyOfRange(hayvanlar1, 2,6);
+        System.out.println(Arrays.toString(yeniHayvalarListOls));
+
+        String [] evcil = new String[]{"Köpek", "Tavsan", "Kedi", "Kus" };
+        String [] yabani = new String[]{"Köpek", "Tavsan", "Kaplan", "Kus" };
+        System.out.println(Arrays.equals(evcil, yabani));
+
+        Arrays.fill(hayvanlar1, "Kedi");
+        System.out.println(Arrays.toString(hayvanlar1));
+
+        List<String> hayvanlarliste = Arrays.asList(hayvanlar);
+        System.out.println("hayvanlarliste = " + hayvanlarliste);
+
+
+    }
 }
--- src/benimcalismalarim/yilbasitatili/Soblonlar.java
+++ src/benimcalismalarim/yilbasitatili/Soblonlar.java
@@ -2,8 +2,8 @@ package benimcalismalarim.yilbasitatili;
 
 public class Soblonlar {
     /*
-    1- ARRAY        ===> String[] names = {"Alice", "Bob", "Charlie"};
-    2- ARRAYLIST    ===> ArrayList<Integer> numbers = new ArrayList<>();
+    1- ARRAY        ===> int [] sayilar = new int[]{80,20,40,50,30};
+    2- ARRAYLIST    ===> ArrayList<Integer> numbers = new ArrayList<>(6);
     3- LIST         ===> List<String> items = new ArrayList<>();
 
 
