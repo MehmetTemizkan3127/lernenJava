9c1700d6f50059596f29c51a5164fffd74d22219
Mehmet <mehmettemizkanabs@gmail.com>
2025-01-09 08:58:30 +0100

    Interface

---
+++ src/j27interface/Lastik.java
@@ -0,0 +1,7 @@
+package j27interface;
+
+public interface Lastik {
+    void lastikEbatı();
+    void jant();
+    String RENK = "Beyaz Çizgili";
+}
---
+++ src/j27interface/Tofask.java
@@ -0,0 +1,10 @@
+package j27interface;
+
+public abstract class Tofask {
+
+    public abstract void motor();
+    public abstract void yakit();
+    public void sunRoof(){
+        System.out.println("Soğuk havalarda problem çıkarmaz mı ? ");
+    };
+}
