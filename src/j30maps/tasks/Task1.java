a05d85bb78baacd4611dfb914d88e48e62f5a3be
Mehmet <mehmettemizkanabs@gmail.com>
2025-01-15 00:16:12 +0100

    Map

---
+++ src/j30maps/tasks/Task1.java
@@ -0,0 +1,23 @@
+package j30maps.tasks;
+
+import javanotlari.Map;
+
+import java.util.HashMap;
+
+public class Task1 {
+    public static void main(String[] args) {
+        /*
+
+        Bir map deki anahtarları ve değerleri takas ederek yeni bir harita oluşturun.
+        Map<String, String> countryCapital = new HashMap<>();
+        countryCapital.put("Turkey", "Ankara");
+        countryCapital.put("Germany", "Berlin");
+        countryCapital.put("France", "Paris");
+
+         */
+
+
+
+
+    }
+}
