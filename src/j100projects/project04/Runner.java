7faa299cbd24d6d1de2290f230c5c6ac6918648b
Mehmet <mehmettemizkanabs@gmail.com>
2025-01-18 22:47:47 +0100

    Film OyunProjesi

---
+++ src/j100projects/project04/Runner.java
@@ -0,0 +1,15 @@
+package j100projects.project04;
+
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Film.menuGiris();
+
+
+    }
+
+}
